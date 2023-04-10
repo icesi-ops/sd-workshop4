87ba71f27941bb5f250101e1771ee3ad7d78b812
Juanferas <juanferas25@hotmail.com>
2023-04-10 18:28:41 -0500

    Consul-template execute working

--- playbooks/scripts/post-provisioning.sh
+++ playbooks/scripts/post-provisioning.sh
@@ -6,6 +6,6 @@ then
 else
   echo -e "[+] Todas las maquinas han sido provisionadas y están corriendo\n\nEjecutando playbook para configurar el consul-template..."
   # Ejecutar playbook consul-template.yml
-  ansible-playbook -v ../consul/consul_template.yml
+  ansible-playbook -v playbooks/consul/consul_template.yml
   echo -e "\n\n[+] Script de post-provisionamiento ejecutado correctamente :)"
 fi
